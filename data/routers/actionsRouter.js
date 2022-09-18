b1e51dcbf37bfc299730c4448f5d9b414b30ae51
Mirage Kamran <mirage.kamran@outlook.com>
2022-09-17 22:33:14 -0400

    helpers added

---
+++ data/routers/actionsRouter.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const router = express.Router();
+// import helper from actionModel
+const actions = require("../helpers/actionModel");
+
+module.exports = router;
